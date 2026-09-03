6edcb85247e389bcba1bc9267e9eac47bb1736ce
m4tthewtan <156554782+matthewtansh@users.noreply.github.com>
2026-09-03 14:42:45 +0800

    feat: todos, events, deadlines

--- src/main/java/Ollie.java
+++ src/main/java/Ollie.java
@@ -28,7 +28,16 @@ public class Ollie {
                     break;
                 }
 
-                if (command.equals("list")) {
+                if (command.equals("help")) {
+                    System.out.println(UI_INDENTATION + "help");
+                    System.out.println(UI_INDENTATION + "list");
+                    System.out.println(UI_INDENTATION + "todo <description>");
+                    System.out.println(UI_INDENTATION + "deadline <description> /by <date>");
+                    System.out.println(UI_INDENTATION + "event <description> /from <date> /to <date>");
+                    System.out.println(UI_INDENTATION + "mark <task number>");
+                    System.out.println(UI_INDENTATION + "unmark <task number>");
+                    System.out.println(UI_INDENTATION + "bye");
+                } else if (command.equals("list")) {
                     System.out.println(UI_INDENTATION + "Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println(UI_INDENTATION + (i + 1) + ". " + tasks.get(i));
@@ -38,20 +47,30 @@ public class Ollie {
                     Task task = tasks.get(taskIndex);
                     task.mark();
                     System.out.println(UI_INDENTATION + "Nice! I've marked this task as done.");
-                    System.out.println(UI_INDENTATION + "  " + task);
                 } else if (command.startsWith("unmark ")) {
                     int taskIndex = Integer.parseInt(command.substring("unmark ".length())) - 1;
                     Task task = tasks.get(taskIndex);
                     task.unmark();
                     System.out.println(UI_INDENTATION + "Nice! I've marked this task as undone.");
-                    System.out.println(UI_INDENTATION + "  " + task);
-                } else {
-                    tasks.add(new Task(command));
-                    System.out.println(UI_INDENTATION + "added: " + command);
+                } else if (command.startsWith("todo ")) {
+                    String description = command.substring("todo ".length());
+                    addTask(tasks, new Todo(description));
+                } else if (command.startsWith("deadline ")) {
+                    String[] deadlineParts = command.substring("deadline ".length()).split(" /by ", 2);
+                    addTask(tasks, new Deadline(deadlineParts[0], deadlineParts[1]));
+                } else if (command.startsWith("event ")) {
+                    String[] eventParts = command.substring("event ".length()).split(" /from ", 2);
+                    String[] timeParts = eventParts[1].split(" /to ", 2);
+                    addTask(tasks, new Event(eventParts[0], timeParts[0], timeParts[1]));
                 }
 
                 System.out.println(UI_HORIZONTAL_LINE);
             }
         }
+    }
+
+    private static void addTask(ArrayList<Task> tasks, Task task) {
+        tasks.add(task);
+        System.out.println(UI_INDENTATION + "Got it. I've added this task.");
     }
 }

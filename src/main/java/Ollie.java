1a88281242d4a66ffccdf379a0fa3c8bc49367b0
m4tthewtan <156554782+matthewtansh@users.noreply.github.com>
2026-09-03 11:05:03 +0800

    feat: echo

--- src/main/java/Ollie.java
+++ src/main/java/Ollie.java
@@ -1,7 +1,8 @@
-/**
- * Runs the Ollie chatbot's initial greeting and farewell.
- */
+import java.util.Scanner;
+
 public class Ollie {
+    private static final String HORIZONTAL_LINE = "------------------------------------------------------------";
+
     public static void main(String[] args) {
         String banner = "  ___  _ _ _      \n"
                 + " / _ \\| | (_) ___ \n"
@@ -9,9 +10,24 @@ public class Ollie {
                 + "| |_| | | | |  __/\n"
                 + " \\___/|_|_|_|\\___|\n";
 
-        System.out.println(banner);
+        System.out.print(banner);
         System.out.println("Hello! I'm Ollie.");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+
+                if (command.equals("bye")) {
+                    System.out.println("    Bye. Hope to see you again soon!");
+                    System.out.println(HORIZONTAL_LINE);
+                    break;
+                }
+
+                System.out.println("    " + command);
+                System.out.println(HORIZONTAL_LINE);
+            }
+        }
     }
 }
